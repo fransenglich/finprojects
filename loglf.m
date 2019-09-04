8b9e34e40f39123a8af341bb5dcd0724fd5a8bef
Frans Englich <fenglich@fastmail.fm>
2019-09-04 22:02:48 +0200

    Today's work.

--- loglf.m
+++ loglf.m
@@ -1,31 +1,33 @@
 
 % An ML LLF using Gaussian.
-%
-% args = conditional variance
-% data = prices, y
-function [retval] = loglf(args, data)
-    cond_var = args;
- 
-    T = length(data);
+function [LL,sigma2] = loglf(theta,r)
 
-    % We do the minuses later on.
-    part1 = (T/2) * log(2 * pi);
+    n = numel(r);
 
-    part2 = 0;
-    for t = 1:T
-        part2 = part2 + log(cond_var(t)); 
+    % We follow Sundstrom (2017): https://www.math.kth.se/matstat/seminarier/reports/M-exjobb17/170908b.pdf
+    %
+    % Section 2.1 defines the model, with 3 parameters: w, alpha, beta)
+    % r(t)      = sigma2(t)*e(t)
+    % sigma2(t) = w + alpha*r(t-1)^2 + beta*sigma2(t-1)
+    % 
+    % Section 3.1.1 describes some clever parameter transformations:
+    % s2   = w / (1 - alpha - beta)
+
+    % It is observed that s2 is the unconditional variance, so:
+    s2 = var(r);
+    alpha = theta(1);
+    beta  = theta(2);
+    w = s2 * (1 - alpha - beta);
+
+    % construct the sigma2 time series:
+    sigma2 = zeros(n,1);
+    sigma2(1) = s2; % use the unconditional variance as initial value here
+    for t = 2:n
+        sigma2(t) = w + alpha*r(t-1)^2 + beta*sigma2(t-1);
     end
-    part2 = part2 / 2;
 
-    part3 = 0;
-    for t = 1:T
-        y_t = data(t);
-        
-        c = y_t - ar_contribution(data, t);
-        part3 = (part3 + (c)^2)/cond_var(t);
-    end
-    part3 = part3 / 2;
-
-    % Here's our complete LLF computed.
-    retval = - part1 - part2 - part3;
+    % Now that we have sigma2, we can compute the log-likelihood, see Sundstrom's equation (23) on page
+    % 13. (Omit the first constant term, and the factors 1/2, because those are irrelevant for
+    % maximization.)
+    LL = -sum(log(sigma2))-sum(r.^2 ./ sigma2);
 end
--- wool_garch_main.m
+++ wool_garch_main.m
@@ -1,10 +1,35 @@
 
+%% ML estimation: 
+% our task is to estimate (as good as possible) the "unknown" parameters
+% s2, alpha, beta, only using the time series r:
 
-%% GARCH(1, 1)
+% initial guess:
+alpha0 = 0.5;
+beta0  = 0.5;
+theta0 = [alpha0; beta0];
 
-% We do minus before since we want to maximize with fminunc().
-data_carrier = @(x) - estimate_garch(x, prices);
+fun = @(x)-loglf(x, prices);
+thetaHat = fmincon(fun,theta0,[],[],[],[],[0;0],[1;1]);
 
-x0 = ones(length(prices), 1);
+% estimated parameters:
+alphaHat = thetaHat(1);
+betaHat  = thetaHat(2);
 
-cond_var = fminunc(data_carrier, x0);
+% the corresponding estimated sigma2:
+[~,sigma2Hat] = loglf(thetaHat, prices);
+
+% plot the result
+figure
+
+subplot(2, 1, 1)
+plot(exp(cumsum(prices)))
+title('Daily prices','Interpreter','latex')
+
+subplot(2,1,2)
+plot(sqrt(250)*sqrt(sigma2))
+hold on
+plot(sqrt(250)*sqrt(sigma2Hat),'r')
+%legend({sprintf('True $(\\alpha,\\beta) = (%3.2f,%3.2f)$',alpha,beta),...
+%        sprintf('Estimated $(\\hat{\\alpha},\\hat{\\beta}) = (%3.2f,%3.2f)$',alphaHat,betaHat)},'Interpreter','latex');
+
+%title('Ann. conditional daily std ($\sigma(t)\sqrt{250}$)','Interpreter','latex')
