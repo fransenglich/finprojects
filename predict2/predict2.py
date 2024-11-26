66f03dcf21ac10d6432df452f7ff928c48c4dfe0
Frans Englich <fenglich@fastmail.fm>
2024-11-26 10:13:57 +0100

    Legends, fix variance.

--- predict2/predict2.py
+++ predict2/predict2.py
@@ -10,23 +10,31 @@ def main():
     closes = numarray[:, 4] # Column "Adj_Close"
     returns = df['Adj_Close'].pct_change()
 
+    plt.title("Prediction explorations")
+
+
     plt.subplot(311)
     plt.plot(closes)
+    plt.legend(["Adjusted closes"])
 
     plt.subplot(312)
     plt.plot(returns)
+    plt.legend(["Returns"])
 
-    window_size = 1
+    window_size = 5
     variance = returns.rolling(window_size).std() * (250 ** 0.5)
 
     plt.subplot(313)
     plt.plot(variance)
+
     print(f"variance: {variance}")
+    print(f"type variance: {type(variance)}")
 
     # Graph stuff
     plt.savefig("output_graph.png")
     plt.savefig("output_graph.svg")
 
+    plt.legend(["Variance"])
     plt.show()
 
 main()
