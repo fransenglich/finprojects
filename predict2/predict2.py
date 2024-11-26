3af06735090e38f13273adcab88f0067f0ae0f86
Frans Englich <fenglich@fastmail.fm>
2024-11-26 09:45:42 +0100

    Column graphs.

--- predict2/predict2.py
+++ predict2/predict2.py
@@ -10,12 +10,20 @@ def main():
     closes = numarray[:, 4] # Column "Adj_Close"
     returns = df['Adj_Close'].pct_change()
 
-    plt.subplot(122)
+    plt.subplot(311)
     plt.plot(closes)
 
-    plt.subplot(121)
+    plt.subplot(312)
     plt.plot(returns)
 
+    window_size = 1
+    variance = returns.rolling(window_size).std() * (250 ** 0.5)
+
+    plt.subplot(313)
+    plt.plot(variance)
+    print(f"variance: {variance}")
+
+    # Graph stuff
     plt.savefig("output_graph.png")
     plt.savefig("output_graph.svg")
 
