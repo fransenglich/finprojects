1d97bcf7ea19bd55a167cc083552710ac35464fc
Frans Englich <fenglich@fastmail.fm>
2024-11-27 11:49:52 +0100

    Polish.

--- predict2/test_ewma.py
+++ predict2/test_ewma.py
@@ -2,13 +2,15 @@ import unittest
 
 import ewma
 
-class TestSum(unittest.TestCase):
+class TestEWMA(unittest.TestCase):
 
     def test_values(self):
         ans = ewma.compute_EWMA((1, 2, 3))
         baseline = (0.06, 0.2964, 0.8186)
 
-        for i in list(range(len(ans))):
+        self.assertEqual(ans, baseline, "")
+
+        for i in range(len(ans)):
             self.assertAlmostEqual(ans[i], baseline[i])
 
 if __name__ == '__main__':
