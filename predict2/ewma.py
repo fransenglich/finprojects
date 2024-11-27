678b7e3edb50648fecede8b2e2a30784531b2d97
Frans Englich <fenglich@fastmail.fm>
2024-11-27 11:53:40 +0100

    Work.

--- predict2/ewma.py
+++ predict2/ewma.py
@@ -1,8 +1,6 @@
 
-
 # Returns a list of the EWMAs (variance) for the returns `returns'
-# mean is assumed to be 0.
-def compute_EWMA(returns, weight = 0.94):
+def compute_EWMA(returns, weight = 0.94, mean = 0):
     print(f"type returns: {type(returns)}")
     print(f"returns: {returns}")
 
@@ -10,7 +8,7 @@ def compute_EWMA(returns, weight = 0.94):
     previous_value = 0
 
     for i in returns:
-        new_value = weight * previous_value + (1 - weight) * (i ** 2)
+        new_value = weight * previous_value + (1 - weight) * ((i - mean) ** 2)
         previous_value = new_value
         retval.append(new_value)
 
--- predict2/test_ewma.py
+++ predict2/test_ewma.py
@@ -6,9 +6,9 @@ class TestEWMA(unittest.TestCase):
 
     def test_values(self):
         ans = ewma.compute_EWMA((1, 2, 3))
-        baseline = (0.06, 0.2964, 0.8186)
+        baseline = (0.06, 0.2964, 0.818616)
 
-        self.assertEqual(ans, baseline, "")
+        #self.assertEqual(ans, baseline, "")
 
         for i in range(len(ans)):
             self.assertAlmostEqual(ans[i], baseline[i])
