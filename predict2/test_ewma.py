5aaa6e6ff05d69285162b9c4c7de2306e6d4848c
Frans Englich <fenglich@fastmail.fm>
2024-11-27 12:36:40 +0100

    SD, incomplete.

--- predict2/test_ewma.py
+++ predict2/test_ewma.py
@@ -8,10 +8,18 @@ class TestEWMA(unittest.TestCase):
         ans = ewma.compute_EWMA((1, 2, 3))
         baseline = (0.06, 0.2964, 0.818616)
 
-        #self.assertEqual(ans, baseline, "")
+        for i in range(len(ans)):
+            self.assertAlmostEqual(ans[i], baseline[i])
+
+class TestSD(unittest.TestCase):
+
+    def test_values(self):
+        ans = ewma.compute_SD((1, 2, 3))
+        baseline = (0.5, 0.5, 0.816)
 
         for i in range(len(ans)):
             self.assertAlmostEqual(ans[i], baseline[i])
+
 
 if __name__ == '__main__':
     unittest.main()
