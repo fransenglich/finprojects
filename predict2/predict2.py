c3917ba59b9deb2be3895303d2733c3a15c26623
Frans Englich <fenglich@fastmail.fm>
2024-12-03 11:55:41 +0100

    Better working OLS.

--- predict2/predict2.py
+++ predict2/predict2.py
@@ -11,7 +11,7 @@ def main():
 
     closes = numarray[:, 4] # Column "Adj_Close"
     returns = df['Adj_Close'].pct_change()
-    print(f"returns: {returns}")
+    #print(f"returns: {returns}")
 
     # Get rid of nan, pct_change() produced it, computing the EWMA fails with it.
     returns[0] = 0
@@ -50,43 +50,46 @@ def main():
     # ----------------------------------- Onwards
     # Now we have our nice time series `returns' and volatility in `ewmas'.
 
+
+    # ----------------------------------- OLS
     # Goal: list of B, predict next value (+1) with the B.
     # Then we have an OLS prediction
 
+    # Our list of regression coefficients
     B = []
 
-    ols_window_size = 20 * 2 # 2 finance months
+    ols_window_size = 20 * 2 # 2 financial months
+    all_time = list(range(len(returns)))
 
-    retlen = len(returns)
-
-    # Discrete time, our X
-    fulltime = list(range(ols_window_size, retlen - ols_window_size))
-    print(f"len(fulltime): {len(fulltime)}")
-    print(f"len(returns): {len(returns)}")
-
-    fulltime2 = list(range(ols_window_size, retlen))
-
-    # A rolling window
-    for i in fulltime:
-        y = returns[i:i + ols_window_size]
-
+    # We do a rolling window and compute an OLS B for every step.
+    for i in range(len(returns) - ols_window_size):
         print(f"i: {i}")
-        print(f"fulltime.len: {len(fulltime)}")
-        X = fulltime2[i:i + ols_window_size]
 
-        print(f"y.len: {len(y)}")
-        print(f"X.len: {len(X)}")
+        y = returns[i + ols_window_size:i + ols_window_size * 2]
+
+        X = all_time[i + ols_window_size:i + ols_window_size * 2]
+
+        print(f"len(y): {len(y)}")
+        print(f"len(X): {len(X)}")
+
+        if len(X) < ols_window_size:
+            break
 
         model = sm.OLS(y, X)
         res = model.fit()
 
+        # We only have one independent, so one coef.
         B.append(res.params.iloc[0])
 
-    print(type(B))
+        print(f"len(y): {len(y)}")
+        print(f"len(X): {len(X)}")
 
-    print(dir(res))
-    print(res.summary())
-    print(f"params: {res.params}")
+    print(f"len(B): {len(B)}")
+    print(f"len(returns): {len(returns)}")
+
+    # y_hat(t) = B * x(t)
+    y_hat = [B[i] * returns[i] for i in range(len(B))]
+
 
     # ----------------------------------- Graph stuff
     plt.savefig("output_graph.png")
