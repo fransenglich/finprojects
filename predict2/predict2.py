f4300ea3e8318db78786d40b105dfd12b1b78b04
Frans Englich <fenglich@fastmail.fm>
2024-12-03 11:14:55 +0100

    Broken version.

--- predict2/predict2.py
+++ predict2/predict2.py
@@ -27,10 +27,10 @@ def main():
     plt.legend(["Returns"])
 
     # Smallest possible to get high resolution. SD is undefined for 1 value.
-    window_size = 2
+    sd_window_size = 2
 
-    print(f"window_size: {window_size}")
-    sd = returns.rolling(window_size).std() * (250 ** 0.5)
+    print(f"sd_window_size: {sd_window_size}")
+    sd = returns.rolling(sd_window_size).std() * (250 ** 0.5)
 
     print(f"sd: {sd}")
     print(f"type sd: {type(sd)}")
@@ -53,14 +53,34 @@ def main():
     # Goal: list of B, predict next value (+1) with the B.
     # Then we have an OLS prediction
 
-    y = returns
+    B = []
 
-    X = list(range(len(returns))) # Discrete time
+    ols_window_size = 20 * 2 # 2 finance months
 
-    model = sm.OLS(y, X)
-    res = model.fit()
+    retlen = len(returns)
 
-    B = res.params.iloc[0]
+    # Discrete time, our X
+    fulltime = list(range(ols_window_size, retlen - ols_window_size))
+    print(f"len(fulltime): {len(fulltime)}")
+    print(f"len(returns): {len(returns)}")
+
+    fulltime2 = list(range(ols_window_size, retlen))
+
+    # A rolling window
+    for i in fulltime:
+        y = returns[i:i + ols_window_size]
+
+        print(f"i: {i}")
+        print(f"fulltime.len: {len(fulltime)}")
+        X = fulltime2[i:i + ols_window_size]
+
+        print(f"y.len: {len(y)}")
+        print(f"X.len: {len(X)}")
+
+        model = sm.OLS(y, X)
+        res = model.fit()
+
+        B.append(res.params.iloc[0])
 
     print(type(B))
 
